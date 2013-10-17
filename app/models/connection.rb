e7bf4b2ccaebc86f4745995d2175db4b1cc50bda
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-10-16 22:04:01 -0700

    Add adapter based on variable, add validation to make sure adapter is in supported adapters

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,14 +1,24 @@
 class Connection < ActiveRecord::Base
-  validates :host, :username, :password, :database, presence: true
+  validates :adapter, :host, :username, :password, :database, presence: true
+  validate :check_adapter
   has_many :connection_tables, :dependent => :destroy
+
+  SUPPORTED_ADAPTERS = %w(mysql postgresql)
 
   def connection_info
     {
-      :adapter  => "mysql",
+      :adapter  => adapter,
       :host     => host,
       :username => username,
       :password => password,
       :database => database
     }
   end
+
+  private
+    def check_adapter
+      unless SUPPORTED_ADAPTERS.include? self.adapter
+        errors[:base] << "The selected adapter is not supported"
+      end
+    end
 end
